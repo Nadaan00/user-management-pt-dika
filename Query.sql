d826a1614c53a12f9ae143b7d5bf11d2279fa18a
Nada Anis Nurjihan <119091984+Nadaan00@users.noreply.github.com>
2025-01-13 22:43:04 +0700

    query sql

---
+++ Query.sql
@@ -0,0 +1,7 @@
+CREATE TABLE category_product (
+    category_id INT,
+    product_id INT,
+    PRIMARY KEY (category_id, product_id),
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
+);
